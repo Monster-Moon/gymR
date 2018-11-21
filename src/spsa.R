cadc133c66c3625cf62fa740a8d2894e0db14861
Monster-Moon <msj5589@gmail.com>
2018-11-21 16:34:32 +0900

    Update spsa.R

--- src/spsa.R
+++ src/spsa.R
@@ -34,6 +34,28 @@ break_option = ifelse(time_step <=200, T, F)
 
 for(k in 1:episode_num)
 {
+
+  ## Update
+  a_k = 1/(k+1)^alpha_val
+  c_k = 1/(k+1)^gamma_val
+  
+  delta_k = sample(c(-1, 1), size = 4, replace = T)
+  beta_plus = beta_init + c_k * delta_k
+  beta_minus = beta_init - c_k * delta_k
+  
+  beta_plus_reward = sum(reward_fun(client, instance_id, 
+                                    time_step = time_step, 
+                                    beta_vec = beta_plus, 
+                                    break_option = break_option))
+  
+  beta_minus_reward = sum(reward_fun(client, instance_id, 
+                                     time_step = time_step, 
+                                     beta_vec = beta_minus, 
+                                     break_option = break_option))
+  
+  gradient_vec = (beta_plus_reward - beta_minus_reward)/(2 * c_k) * delta_k
+  beta_init = beta_init - a_k * gradient_vec
+  
   ## Stopping rule
   episode_reward_val = reward_fun(client, instance_id, 
                                   beta_vec = beta_init, 
@@ -64,26 +86,6 @@ for(k in 1:episode_num)
   }
   if(complete_stack == complete_stack_lim) break
   
-  ## Update
-  a_k = 1/(k+1)^alpha_val
-  c_k = 1/(k+1)^gamma_val
-  
-  delta_k = sample(c(-1, 1), size = 4, replace = T)
-  beta_plus = beta_init + c_k * delta_k
-  beta_minus = beta_init - c_k * delta_k
-  
-  beta_plus_reward = sum(reward_fun(client, instance_id, 
-                                    time_step = time_step, 
-                                    beta_vec = beta_plus, 
-                                    break_option = break_option))
-  
-  beta_minus_reward = sum(reward_fun(client, instance_id, 
-                                     time_step = time_step, 
-                                     beta_vec = beta_minus, 
-                                     break_option = break_option))
-  
-  gradient_vec = (beta_plus_reward - beta_minus_reward)/(2 * c_k) * delta_k
-  beta_init = beta_init - a_k * gradient_vec
 }
 
 reward_fun(client, instance_id, beta_vec = beta_star, time_step = 500, render_display = T, break_option = F)
