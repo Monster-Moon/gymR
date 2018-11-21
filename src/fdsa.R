64f2fe637fcefc5dfdd6a4127ec55c3e4cb1d8f4
Monster-Moon <msj5589@gmail.com>
2018-11-21 16:34:30 +0900

    Update fdsa.R

--- src/fdsa.R
+++ src/fdsa.R
@@ -34,6 +34,31 @@ break_option = ifelse(time_step <=200, T, F)
 
 for(k in 1:episode_num)
 {
+  ## Update
+  a_k = 1/ (k+1)^alpha_val
+  c_k = 1/ (k+1)^gamma_val
+  
+  plus_minus_mat = c_k * diag(1, nrow = observation_dim, ncol = observation_dim)
+  beta_plus_mat = matrix(beta_init, nrow = observation_dim, ncol = observation_dim) + plus_minus_mat
+  beta_minus_mat = matrix(beta_init, nrow = observation_dim, ncol = observation_dim) - plus_minus_mat
+  
+  beta_plus_reward_list = lapply(1:observation_dim, 
+                                 function(i) reward_fun(client, instance_id, 
+                                                        time_step = time_step, 
+                                                        beta_vec = beta_plus_mat[, i], 
+                                                        break_option = break_option))
+  beta_minus_reward_list = lapply(1:observation_dim, 
+                                  function(i) reward_fun(client, instance_id, 
+                                                         time_step = time_step, 
+                                                         beta_vec = beta_minus_mat[, i], 
+                                                         break_option = break_option))
+  
+  beta_plus_reward = lapply(beta_plus_reward_list, 'sum') %>% unlist()
+  beta_minus_reward = lapply(beta_minus_reward_list, 'sum') %>% unlist()
+  
+  gradient_vec = (beta_plus_reward - beta_minus_reward)/(2 * c_k)
+  beta_init = beta_init - a_k * gradient_vec
+  
   ## Stopping rule
   episode_reward_val = reward_fun(client, instance_id, 
                                   beta_vec = beta_init, 
@@ -64,30 +89,6 @@ for(k in 1:episode_num)
   }
   if(complete_stack == complete_stack_lim) break
   
-  ## Update
-  a_k = 1/ (k+1)^alpha_val
-  c_k = 1/ (k+1)^gamma_val
-  
-  plus_minus_mat = c_k * diag(1, nrow = observation_dim, ncol = observation_dim)
-  beta_plus_mat = matrix(beta_init, nrow = observation_dim, ncol = observation_dim) + plus_minus_mat
-  beta_minus_mat = matrix(beta_init, nrow = observation_dim, ncol = observation_dim) - plus_minus_mat
-  
-  beta_plus_reward_list = lapply(1:observation_dim, 
-                                 function(i) reward_fun(client, instance_id, 
-                                                        time_step = time_step, 
-                                                        beta_vec = beta_plus_mat[, i], 
-                                                        break_option = break_option))
-  beta_minus_reward_list = lapply(1:observation_dim, 
-                                  function(i) reward_fun(client, instance_id, 
-                                                         time_step = time_step, 
-                                                         beta_vec = beta_minus_mat[, i], 
-                                                         break_option = break_option))
-  
-  beta_plus_reward = lapply(beta_plus_reward_list, 'sum') %>% unlist()
-  beta_minus_reward = lapply(beta_minus_reward_list, 'sum') %>% unlist()
-  
-  gradient_vec = (beta_plus_reward - beta_minus_reward)/(2 * c_k)
-  beta_init = beta_init - a_k * gradient_vec
 }
 
 reward_fun(client, instance_id, beta_vec = beta_star, time_step = 500, render_display = T, break_option = F)
